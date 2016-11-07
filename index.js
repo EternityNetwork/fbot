50fcc4fa34c30601f149c768b4f9009da98949fb
EternityNetwork <contact@eternitynetwork.net>
2016-11-07 19:29:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post('/webhook/', function (req, res) {
 		let event = req.body.entry[0].messaging[i]
 		let sender = event.sender.id
 		if (event.message && event.message.text) {
-			let text = event.message.text
+			let text = event.message.text.toLowerCase()
 			if (text.includes("Demos")) {
 				sendDemosMessage(sender)
 				continue
