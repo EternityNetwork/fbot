7ace4bc9ad32cdf0d546021698bc9877c4845e30
EternityNetwork <contact@eternitynetwork.net>
2016-11-07 19:12:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -128,7 +128,6 @@ function sendTextMessage(sender, text) {
 		method: 'POST',
 		json: {
 			recipient: {id:sender},
-			sender_action:typing_on,
 			message: messageData,
 		}
 	}, function(error, response, body) {
